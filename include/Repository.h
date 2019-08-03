9edf7871040b584730e10df6ee259fc900585f63
James Ding <james.ding.illinois@gmail.com>
2019-08-03 15:14:22 -0500

    Repository header inclusion procedure optimization

--- include/Repository.h
+++ include/Repository.h
@@ -1,11 +1,6 @@
 #ifndef REPOSITORY_H
 #define REPOSITORY_H
 
-#include <Poco/JSON/JSON.h>
-#include <Poco/JSON/Object.h>
-#include <Poco/JSON/Stringifier.h>
-#include <Poco/Dynamic/Var.h>
-
 #include <Poco/Data/MySQL/Connector.h>
 #include <Poco/Data/Time.h>
 #include <Poco/Data/Session.h>
