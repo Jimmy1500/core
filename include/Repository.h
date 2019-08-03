0b5801115186c21bf5794dc8969a077e617af42d
James Ding <james.ding.illinois@gmail.com>
2019-08-03 15:13:17 -0500

    Repository abstraction for database manipulation

---
+++ include/Repository.h
@@ -0,0 +1,19 @@
+#ifndef REPOSITORY_H
+#define REPOSITORY_H
+
+#include <Poco/JSON/JSON.h>
+#include <Poco/JSON/Object.h>
+#include <Poco/JSON/Stringifier.h>
+#include <Poco/Dynamic/Var.h>
+
+#include <Poco/Data/MySQL/Connector.h>
+#include <Poco/Data/Time.h>
+#include <Poco/Data/Session.h>
+#include <Poco/Data/SessionPool.h>
+#include <Poco/Data/SessionFactory.h>
+#include <Poco/Data/MySQL/MySQLException.h>
+
+#include "Global.h"
+using namespace std;
+
+#endif
--- include/RequestHandler.h
+++ include/RequestHandler.h
@@ -10,24 +10,11 @@
 #include <Poco/Net/HTTPServerResponse.h>
 #include <Poco/Util/ServerApplication.h>
 
-#include <Poco/JSON/JSON.h>
-#include <Poco/JSON/Object.h>
-#include <Poco/JSON/Stringifier.h>
-#include <Poco/Dynamic/Var.h>
-
-#include <Poco/Data/Time.h>
-#include <Poco/Data/MySQL/Connector.h>
-#include <Poco/Data/Session.h>
-#include <Poco/Data/SessionPool.h>
-#include <Poco/Data/SessionFactory.h>
-#include <Poco/Data/MySQL/MySQLException.h>
-
-#include "Global.h"
+#include "Repository.h"
 
 using namespace Poco::Net;
 using namespace Poco::Util;
 using namespace Poco::JSON;
-using namespace Poco::Data;
 using namespace std;
 
 class RequestHandler : public HTTPRequestHandler {
